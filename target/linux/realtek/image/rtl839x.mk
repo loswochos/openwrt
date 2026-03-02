9bcdb241c80a953a370f73a80002912219ad3ac5
Klaus Rubenstein <klaus.rubenstein@gmail.com>
2026-03-02 13:11:12 +0100

    realtek: add Zyxel GS1900-48HP v1 support
    Add support for the Zyxel GS1900-48HP v1 managed PoE switch based on
    RTL8393 SoC with 48 copper ports (6x RTL8218B), 2 SFP slots and PoE
    (170W budget). Includes DTS, image definition, network config and
    u-boot-env support.

--- target/linux/realtek/image/rtl839x.mk
+++ target/linux/realtek/image/rtl839x.mk
@@ -98,6 +98,16 @@ define Device/zyxel_gs1900-48-a1
 endef
 TARGET_DEVICES += zyxel_gs1900-48-a1
 
+define Device/zyxel_gs1900-48hp-v1
+  $(Device/zyxel_gs1900)
+  SOC := rtl8393
+  DEVICE_MODEL := GS1900-48HP
+  ZYXEL_VERS := AAHO
+  SUPPORTED_DEVICES += zyxel,gs1900-48hp-v1
+  DEVICE_PACKAGES += realtek-poe
+endef
+TARGET_DEVICES += zyxel_gs1900-48hp-v1
+
 define Device/zyxel_gs1920-24hp
 ifeq ($(IB),)
   ARTIFACTS := loader.bin
